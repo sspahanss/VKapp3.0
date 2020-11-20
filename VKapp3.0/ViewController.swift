5e7b863ae959a896b94059306bf7af416d706f49
Parcifal <scar1987@gmail.com>
2020-11-20 12:32:30 +0800

    Initial Commit

---
+++ VKapp3.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKapp3.0
+//
+//  Created by Павел on 20.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
